fd6c770c6c85e86c6c154da8174871b2d229af24
Aida Palacio <aidaph@ifca.unican.es>
2026-05-06 15:22:17 +0200

    Add test planner instead of nuclei

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -1,10 +1,10 @@
-from driftmux.scanners.nuclei import NucleiScanner
 from driftmux.models import OpenPort
+from driftmux.planner import build_url
 
 
 def test_build_target_http_and_https():
-    scanner = NucleiScanner()
     svc_http = OpenPort(port=80, protocol="tcp", state="open", service="http")
     svc_https = OpenPort(port=443, protocol="tcp", state="open", service="https", tunnel="ssl")
-    assert scanner.scan_many("example.org", svc_http) == "http://example.org:80"
-    assert scanner.scan_many("example.org", svc_https) == "https://example.org:443"
+
+    assert build_url("example.org", svc_http) == "http://example.org:80"
+    assert build_url("example.org", svc_https) == "https://example.org:443"
